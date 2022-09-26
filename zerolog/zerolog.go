14eed442877b2fff1815f97b869b57d503500ece
Víctor Roldán Betancort <vroldanbet@authzed.com>
2022-09-26 15:21:01 +0100

    introduces a fluent zerolog cobrautil package
    the functionality is largely the same as v1.
    The gist of this redesign is configurability: v1
    didn't offer any API to have cobrautil consumer
    adjust the logger was configured. This is important
    as zerolog provides a vast API with many knobs to tweak.
    
    - a new fluent API is offered so that cobrautil/zerolog can
      be extended with options as the need arises. It's used
      to implement two new options in the package
    - v1 assumed the consumer would use the zerolog/log global
      instance package. This forces the consuming application to use
      that global instance. The WithTarget option offers a callback when
      the logger is instantiated, so e.g. consumers can cache the
      instance somewhere else
    - v1 used zerolog global level. This is an unexpected side-effect
      the consumer may not want, specially given cobrautil is creating
      an instance of a zerolog Logger. The implementation now adjusts the
      level on the logger instance instead.
    - singleton methods are provided to offer an easy path of migration
      to v2.
    - WithAsync configures the logger to be non-blocking, implemented using
      diode buffered-ring. This is default by default to retain v1 behaviour.

--- zerolog/zerolog.go
+++ zerolog/zerolog.go
@@ -2,17 +2,46 @@ package zerolog
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/jzelinskie/cobrautil/v2"
 	"github.com/jzelinskie/stringz"
 	"github.com/mattn/go-isatty"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/diode"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
+
+// ConfigureFunc is a function used to configure this CobraUtil
+type ConfigureFunc = func(cu *CobraUtil)
+
+// New creates a configuration that exposes RegisterZeroLogFlags and ZeroLogRunE
+// to integrate with cobra
+func New(flagPrefix string, configurations ...ConfigureFunc) *CobraUtil {
+	cu := CobraUtil{
+		flagPrefix:  flagPrefix,
+		preRunLevel: zerolog.DebugLevel,
+	}
+	for _, configure := range configurations {
+		configure(&cu)
+	}
+	return &cu
+}
+
+// CobraUtil carries the configuration for a zerolog CobraRunFunc
+type CobraUtil struct {
+	flagPrefix        string
+	target            func(zerolog.Logger)
+	async             bool
+	asyncSize         int
+	asyncPollInterval time.Duration
+	preRunLevel       zerolog.Level
+}
 
 // RegisterZeroLogFlags adds flags for use in with ZeroLogPreRunE:
 // - "$PREFIX-level"
@@ -29,39 +58,100 @@ func RegisterZeroLogFlags(flags *pflag.FlagSet, flagPrefix string) {
 // The required flags can be added to a command by using
 // RegisterLoggingPersistentFlags().
 func ZeroLogRunE(flagPrefix string, prerunLevel zerolog.Level) cobrautil.CobraRunFunc {
-	prefixed := cobrautil.PrefixJoiner(stringz.DefaultEmpty(flagPrefix, "log"))
+	return New(flagPrefix, WithPreRunLevel(prerunLevel)).RunE()
+}
+
+// RegisterFlags adds flags for use in with ZeroLogPreRunE:
+// - "$PREFIX-level"
+// - "$PREFIX-format"
+func (cu CobraUtil) RegisterFlags(flags *pflag.FlagSet) {
+	RegisterZeroLogFlags(flags, cu.flagPrefix)
+}
+
+// RunE returns a Cobra run func that configures the corresponding
+// log level from a command.
+//
+// The required flags can be added to a command by using
+// RegisterLoggingPersistentFlags().
+func (cu CobraUtil) RunE() cobrautil.CobraRunFunc {
+	prefixed := cobrautil.PrefixJoiner(stringz.DefaultEmpty(cu.flagPrefix, "log"))
 	return func(cmd *cobra.Command, args []string) error {
 		if cobrautil.IsBuiltinCommand(cmd) {
 			return nil // No-op for builtins
 		}
 
+		var output io.Writer
+
 		format := cobrautil.MustGetString(cmd, prefixed("format"))
 		if format == "console" || format == "auto" && isatty.IsTerminal(os.Stdout.Fd()) {
-			log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+			output = zerolog.ConsoleWriter{Out: os.Stderr}
+		} else {
+			output = os.Stderr
 		}
+
+		if cu.async {
+			output = diode.NewWriter(output, 1000, 10*time.Millisecond, func(missed int) {
+				fmt.Printf("Logger Dropped %d messages", missed)
+			})
+		}
+
+		l := zerolog.New(output).With().Timestamp().Logger()
 
 		level := strings.ToLower(cobrautil.MustGetString(cmd, prefixed("level")))
 		switch level {
 		case "trace":
-			zerolog.SetGlobalLevel(zerolog.TraceLevel)
+			l = l.Level(zerolog.TraceLevel)
 		case "debug":
-			zerolog.SetGlobalLevel(zerolog.DebugLevel)
+			l = l.Level(zerolog.DebugLevel)
 		case "info":
-			zerolog.SetGlobalLevel(zerolog.InfoLevel)
+			l = l.Level(zerolog.InfoLevel)
 		case "warn":
-			zerolog.SetGlobalLevel(zerolog.WarnLevel)
+			l = l.Level(zerolog.WarnLevel)
 		case "error":
-			zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+			l = l.Level(zerolog.ErrorLevel)
 		case "fatal":
-			zerolog.SetGlobalLevel(zerolog.FatalLevel)
+			l = l.Level(zerolog.FatalLevel)
 		case "panic":
-			zerolog.SetGlobalLevel(zerolog.PanicLevel)
+			l = l.Level(zerolog.PanicLevel)
 		default:
 			return fmt.Errorf("unknown log level: %s", level)
 		}
 
-		log.WithLevel(prerunLevel).Str("new level", level).Msg("set log level")
+		if cu.target != nil {
+			cu.target(l)
+		} else {
+			log.Logger = l
+		}
 
+		l.WithLevel(cu.preRunLevel).
+			Str("format", format).
+			Str("log_level", level).
+			Bool("async", cu.async).
+			Msg("configured zerolog")
 		return nil
+	}
+}
+
+// WithPreRunLevel defines the logging level used for pre-run log messages. Debug by default.
+func WithPreRunLevel(preRunLevel zerolog.Level) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.preRunLevel = preRunLevel
+	}
+}
+
+// WithAsync enables non-blocking logging. Size of the buffer and polling interval can be configured.
+// Disabled by default.
+func WithAsync(size int, pollInterval time.Duration) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.async = true
+		cu.asyncSize = size
+		cu.asyncPollInterval = pollInterval
+	}
+}
+
+// WithTarget callback that forwards the configured logger. Useful when we want to keep it in a global variable.
+func WithTarget(fn func(zerolog.Logger)) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.target = fn
 	}
 }
