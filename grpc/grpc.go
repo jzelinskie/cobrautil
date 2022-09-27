d133cf110a51d8f10751610d40da39271477dede
Víctor Roldán Betancort <vroldanbet@authzed.com>
2022-09-27 11:31:15 +0100

    refactor gRPC package to follow v2 design
    - turned all optional arguments into a ConfigureFunc
    - migrated go go-log/logr facade

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -6,15 +6,45 @@ import (
 	"time"
 
 	"github.com/jzelinskie/cobrautil/v2"
+
+	"github.com/go-logr/logr"
 	"github.com/jzelinskie/stringz"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
 )
+
+// ConfigureFunc is a function used to configure this CobraUtil
+type ConfigureFunc = func(cu *CobraUtil)
+
+// New creates a configuration that exposes RegisterFlags and RunE
+// to integrate with cobra
+func New(serviceName string, configurations ...ConfigureFunc) *CobraUtil {
+	cu := CobraUtil{
+		serviceName:    stringz.DefaultEmpty(serviceName, "grpc"),
+		preRunLevel:    1,
+		logger:         logr.Discard(),
+		defaultAddr:    ":50051",
+		defaultEnabled: false,
+		flagPrefix:     "grpc",
+	}
+	for _, configure := range configurations {
+		configure(&cu)
+	}
+	return &cu
+}
+
+// CobraUtil carries the configuration for a otel CobraRunFunc
+type CobraUtil struct {
+	flagPrefix     string
+	serviceName    string
+	defaultAddr    string
+	defaultEnabled bool
+	logger         logr.Logger
+	preRunLevel    int
+}
 
 // RegisterGrpcServerFlags adds the following flags for use with
 // GrpcServerFromFlags:
@@ -35,10 +65,26 @@ func RegisterGrpcServerFlags(flags *pflag.FlagSet, flagPrefix, serviceName, defa
 	flags.Bool(prefixed("enabled"), defaultEnabled, "enable "+serviceName+" gRPC server")
 }
 
+// RegisterGrpcServerFlags adds the following flags for use with
+// GrpcServerFromFlags:
+// - "$PREFIX-addr"
+// - "$PREFIX-tls-cert-path"
+// - "$PREFIX-tls-key-path"
+// - "$PREFIX-max-conn-age"
+func (cu CobraUtil) RegisterGrpcServerFlags(flags *pflag.FlagSet) {
+	RegisterGrpcServerFlags(flags, cu.flagPrefix, cu.serviceName, cu.defaultAddr, cu.defaultEnabled)
+}
+
 // ServerFromFlags creates an *grpc.Server as configured by the flags from
 // RegisterGrpcServerFlags().
 func ServerFromFlags(cmd *cobra.Command, flagPrefix string, opts ...grpc.ServerOption) (*grpc.Server, error) {
-	prefixed := cobrautil.PrefixJoiner(stringz.DefaultEmpty(flagPrefix, "grpc"))
+	return New("", WithFlagPrefix(flagPrefix)).ServerFromFlags(cmd, opts...)
+}
+
+// ServerFromFlags creates an *grpc.Server as configured by the flags from
+// RegisterGrpcServerFlags().
+func (cu CobraUtil) ServerFromFlags(cmd *cobra.Command, opts ...grpc.ServerOption) (*grpc.Server, error) {
+	prefixed := cobrautil.PrefixJoiner(cu.flagPrefix)
 
 	opts = append(opts, grpc.KeepaliveParams(keepalive.ServerParameters{
 		MaxConnectionAge: cobrautil.MustGetDuration(cmd, prefixed("max-conn-age")),
@@ -49,7 +95,7 @@ func ServerFromFlags(cmd *cobra.Command, flagPrefix string, opts ...grpc.ServerO
 
 	switch {
 	case certPath == "" && keyPath == "":
-		log.Warn().Str("prefix", flagPrefix).Msg("grpc server serving plaintext")
+		cu.logger.V(cu.preRunLevel).Info("grpc server serving plaintext", "prefix", cu.flagPrefix)
 		return grpc.NewServer(opts...), nil
 
 	case certPath != "" && keyPath != "":
@@ -63,16 +109,22 @@ func ServerFromFlags(cmd *cobra.Command, flagPrefix string, opts ...grpc.ServerO
 	default:
 		return nil, fmt.Errorf(
 			"failed to start gRPC server: must provide both --%s-tls-cert-path and --%s-tls-key-path",
-			flagPrefix,
-			flagPrefix,
+			cu.flagPrefix,
+			cu.flagPrefix,
 		)
 	}
 }
 
 // ListenFromFlags listens on an gRPC server using the configuration stored
 // in the cobra command that was registered with RegisterGrpcServerFlags.
-func ListenFromFlags(cmd *cobra.Command, flagPrefix string, srv *grpc.Server, level zerolog.Level) error {
-	prefixed := cobrautil.PrefixJoiner(stringz.DefaultEmpty(flagPrefix, "grpc"))
+func ListenFromFlags(cmd *cobra.Command, flagPrefix string, srv *grpc.Server, preRunLevel int) error {
+	return New("", WithPreRunLevel(preRunLevel), WithFlagPrefix(flagPrefix)).ListenFromFlags(cmd, srv)
+}
+
+// ListenFromFlags listens on an gRPC server using the configuration stored
+// in the cobra command that was registered with RegisterGrpcServerFlags.
+func (cu CobraUtil) ListenFromFlags(cmd *cobra.Command, srv *grpc.Server) error {
+	prefixed := cobrautil.PrefixJoiner(cu.flagPrefix)
 
 	if !cobrautil.MustGetBool(cmd, prefixed("enabled")) {
 		return nil
@@ -86,15 +138,51 @@ func ListenFromFlags(cmd *cobra.Command, flagPrefix string, srv *grpc.Server, le
 		return fmt.Errorf("failed to listen on addr for gRPC server: %w", err)
 	}
 
-	log.WithLevel(level).
-		Str("addr", addr).
-		Str("network", network).
-		Str("prefix", flagPrefix).
-		Msg("grpc server started listening")
+	cu.logger.V(cu.preRunLevel).Info(
+		"grpc server started listening",
+		"addr", addr,
+		"network", network,
+		"prefix", cu.flagPrefix)
 
 	if err := srv.Serve(l); err != nil {
 		return fmt.Errorf("failed to serve gRPC: %w", err)
 	}
 
 	return nil
+}
+
+// WithLogger defines the logger used to log messages in this package
+func WithLogger(logger logr.Logger) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.logger = logger
+	}
+}
+
+// WithDefaultAddress defines the default value of the address the server will listen at.
+// Defaults to ":50051"
+func WithDefaultAddress(addr string) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.defaultAddr = addr
+	}
+}
+
+// WithDefaultEnabled defines whether the gRPC server is enabled by default. Defaults to "false".
+func WithDefaultEnabled(enabled bool) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.defaultEnabled = enabled
+	}
+}
+
+// WithFlagPrefix defines prefix used with the generated flags. Defaults to "grpc".
+func WithFlagPrefix(flagPrefix string) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.flagPrefix = flagPrefix
+	}
+}
+
+// WithPreRunLevel defines the logging level used for pre-run log messages. Defaults to "debug".
+func WithPreRunLevel(preRunLevel int) ConfigureFunc {
+	return func(cu *CobraUtil) {
+		cu.preRunLevel = preRunLevel
+	}
 }
